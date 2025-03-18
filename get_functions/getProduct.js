3cd876b6d12074b3f536f8ffb7aaecd969feddcb
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2025-03-18 11:18:34 +0100

    corrige

--- get_functions/getProduct.js
+++ get_functions/getProduct.js
@@ -6,7 +6,7 @@ const getProduct = async (req, res) => {
     const result = await pool.query(`SELECT products.*, categories.name AS categories_name, colors.name AS colors_name, materials.name AS materials_name 
              FROM products 
              INNER JOIN categories ON products.category_id = categories.id 
-             INNER JOIN colors ON products.colour_id = colors.id 
+             INNER JOIN colors ON products.color_id = colors.id 
              INNER JOIN materials ON products.material_id = materials.id 
              WHERE products.id = $1`, 
             [productId]);
